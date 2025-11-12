7558d5c1a8c746e085f9379e2291a7e55403718c
JabloZ <jablozkontakt@gmail.com>
2025-11-12 18:47:31 +0100

    zmiany w sprawdzanie

--- projekt.cpp
+++ projekt.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
 using namespace std;
-
+int czy_dodatnia(int n){
+	if (n>0){
+		return 1;}
+	else{
+		return 0;}
+}
 int main(){
 	int n;
 	cin>>n;
-	cout<<n;
+	cout<<n<<endl;
+	if (czy_dodatnia(n)==1){
+		cout<<"dodatnia"<<endl;}
+	else{
+		cout<<"niedodatnia"<<endl;}
 	return 0;}
