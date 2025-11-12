2d268ea460531bbe30d7dc89d613aa6ec0cc256c
JabloZ <jablozkontakt@gmail.com>
2025-11-12 18:36:26 +0100

    zmiany w obliczenia

--- projekt.cpp
+++ projekt.cpp
@@ -1,8 +1,21 @@
 #include <iostream>
 using namespace std;
-
+int czy_pierwsza(int n){
+	for (int i=2; i<n;i++){
+		if (n%i==0){
+			return 0;}
+	
+	}
+	return 1;
+}
 int main(){
 	int n;
 	cin>>n;
-	cout<<n;
+	cout<<n<<endl;
+	if (czy_pierwsza(n)==1){
+		cout<<"Pierwsza"<<endl;
+	}
+	else{
+		cout<<"Niepierwsza"<<endl;	
+	}
 	return 0;}
